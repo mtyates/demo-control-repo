5c60f29dbd61b887a4abb935ddf29c6c60fc282c
mtyates <matthewtyleryates@gmail.com>
2017-01-21 21:05:44 -0500

    trying a different image_id.

--- site/profile/manifests/cloud/aws/linuxnode.pp
+++ site/profile/manifests/cloud/aws/linuxnode.pp
@@ -1,7 +1,7 @@
 define profile::cloud::aws::linuxnode (
   $nodename = $title,
   $availability_zone = 'us-east-1a',
-  $image_id = 'ami-775e4f16',
+  $image_id = 'ami-b63769a1',
   $region = 'us-east-1',
   $instance_type = 'm3.medium',
   $security_groups = ['tse-us-east-1-agents', 'tse-us-east-1-master', 'tse-us-east-1-crossconnect'],
